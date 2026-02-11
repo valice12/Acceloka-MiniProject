fe3c2f7b93cf2864b91989106c636e00ef0ce577
valice12 <calvinaritamaga@gmail.com>
2026-02-11 14:34:29 +0700

    UpdateCRUD

--- Acceloka/Program.cs
+++ Acceloka/Program.cs
@@ -4,19 +4,13 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-
-builder.Services.AddEntityFrameworkSqlServer();
 builder.Services.AddDbContextPool<AccelokaContext>(options =>
 {
     var conString = configuration.GetConnectionString("DefaultConnection");
     options.UseSqlServer(conString);
-
 });
 
 var app = builder.Build();
@@ -28,7 +22,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
